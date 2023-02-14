ad85601b7e3223978a781b96e3e963d90de99c28
Lucas Meijer <lucas@unity3d.com>
2023-02-14 23:15:38 +0100

    with openai route

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
+using OpenAI.GPT3.Extensions;
+using OpenAI.GPT3.Interfaces;
+using OpenAI.GPT3.ObjectModels;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddOpenAIService();
 
 var app = builder.Build();
+var openAiService = app.Services.GetRequiredService<IOpenAIService>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,13 +19,27 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.MapGet("/poem", async () =>
+{
+    var completionResult = await openAiService.Completions.CreateCompletion(new()
+    {
+        Prompt = "Once upon a time",
+        Model = Models.TextDavinciV3
+    });
+
+    if (completionResult.Successful)
+        return Results.Text(completionResult.Choices.FirstOrDefault()?.Text);
+    
+    if (completionResult.Error == null)
+        throw new Exception("Unknown Error");
+
+    return Results.Problem($"{completionResult.Error.Code}: {completionResult.Error.Message}");
+});
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
+    
